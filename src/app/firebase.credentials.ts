cade03640b08c410f35637990d606085bbeec808
unknown <gutodalkimin@gmail.com>
2018-03-06 16:47:02 -0300

    Primeiro UP

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyATYitzVzUfByUxIVME6e7aQrBParEaOSU",
+    authDomain: "emprebauru.firebaseapp.com",
+    databaseURL: "https://emprebauru.firebaseio.com",
+    projectId: "emprebauru",
+    storageBucket: "emprebauru.appspot.com",
+    messagingSenderId: "196336474354"
+}
---
+++ src/models/shopping-item/shopping-item.interface.ts
@@ -0,0 +1,9 @@
+export interface ShoppingItem {
+    $key?: string,
+    itemName: string;
+    itemDesc: string;
+    itemNumber: any;
+    itemIcon: string;
+    itemValor: any;
+    itemEmail: any;
+}
